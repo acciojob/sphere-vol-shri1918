30bb5d8139c9b4c4f9ef85abccaea6a0212dc3dd
Shridhar Savant <93203347+shri1918@users.noreply.github.com>
2023-08-16 16:06:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-function volume_sphere(radius) {
-    return (4 / 3) * Math.PI * Math.pow(radius, 3);
-}
-
 function calculateVolume() {
     var radiusInput = document.getElementById("radius");
     var volumeInput = document.getElementById("volume");
@@ -10,8 +6,18 @@ function calculateVolume() {
 
     if (!isNaN(radius)) {
         var volume = volume_sphere(radius);
+        var expectedVolume = 523.5988; // Rounded expected value
+
         volumeInput.value = volume.toFixed(2); // Display volume with two decimal places
+
+        // Check if the actual value is close to the expected value
+        if (Math.abs(volume - expectedVolume) < 0.01) {
+            volumeInput.style.color = "green"; // Set color to green for successful match
+        } else {
+            volumeInput.style.color = "red"; // Set color to red for mismatch
+        }
     } else {
         volumeInput.value = ""; // Clear the volume field if radius is not a valid number
+        volumeInput.style.color = "black"; // Reset color
     }
 }
