eef42e517c56210f18727355998271d3f3bb6f27
Shridhar Savant <93203347+shri1918@users.noreply.github.com>
2023-08-16 15:45:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-function volume_sphere() {
-    //Write your code here
-  
-} 
+// Function to calculate the volume of a sphere
+function volume_sphere(radius) {
+    return (4 / 3) * Math.PI * Math.pow(radius, 3);
+}
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+// Function to update the volume field with calculated volume
+function calculateVolume() {
+    var radiusInput = document.getElementById("radius");
+    var volumeInput = document.getElementById("volume");
+
+    var radius = parseFloat(radiusInput.value);
+
+    if (!isNaN(radius)) {
+        var volume = volume_sphere(radius);
+        volumeInput.value = volume.toFixed(2); // Display volume with two decimal places
+    } else {
+        volumeInput.value = ""; // Clear the volume field if radius is not a valid number
+    }
+}
